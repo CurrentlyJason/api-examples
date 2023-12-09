82ff70d415bde6b2546fbbaa29f02ee4c26e91bd
CurrentlyJason <88570664+CurrentlyJason@users.noreply.github.com>
2023-12-09 11:22:01 +0700

    Update index.sh

--- curl/index.sh
+++ curl/index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-API_KEY=${1:-XXX}
+API_KEY=$MjUzNzBiMTQtYmJiYS00YTg4LWI1NTYtMTBlMmZiOGFhYTdj
 echo "API KEY = $API_KEY"
 
 curl -H "X-Api-Key: $API_KEY" https://api.clockify.me/api/v1/user | json_pp
